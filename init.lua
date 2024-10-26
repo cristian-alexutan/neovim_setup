65369cab3ccddbcc4649086687f8fb4a525294e2
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2024-10-26 16:38:59 +0300

    Added nvim-tree and cmp

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 -- Set statusline with mode and file information
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.signcolumn = "yes"
+
+-- Set statusline with mode and file information
 local current_mode = {
     n  = 'NORMAL ',
     v  = 'VISUAL ',
@@ -48,20 +54,151 @@ vim.opt.scrolloff = 9
 -- Save on Ctrl+S (Normal and Insert modes)
 vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Tab>', '<C-w>w', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-t>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 -- Plugin management using vim-plug
 vim.cmd [[
   call plug#begin()
   
-  " nvim-treesitter for syntax highlighting and code parsing
-  Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+    " nvim-treesitter for syntax highlighting and code parsing
+    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 
-  " catppuccin theme
-  Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+    " catppuccin theme
+    Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+
+    Plug 'nvim-tree/nvim-web-devicons' " optional
+    Plug 'nvim-tree/nvim-tree.lua'
+    Plug 'neovim/nvim-lspconfig'
+
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/cmp-path'
+    Plug 'hrsh7th/cmp-cmdline'
+    Plug 'hrsh7th/nvim-cmp'
 
   call plug#end()
 ]]
 
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
 -- Set colorscheme
 vim.cmd [[colorscheme catppuccin-frappe]]
 
+-- Open nvim-tree automatically in new tabs
+vim.api.nvim_create_autocmd("TabNew", {
+  callback = function()
+    -- Toggle nvim-tree in the new tab
+    require("nvim-tree.api").tree.toggle({ focus = false })
+  end
+})
+
+-- Ensure nvim-tree opens on the left in new tabs
+vim.api.nvim_create_autocmd("TabEnter", {
+  callback = function()
+    -- If nvim-tree isn't open, open it
+    if not require("nvim-tree.view").is_visible() then
+      require("nvim-tree.api").tree.toggle({ focus = false })
+    end
+  end
+})
+
+-- Set up nvim-cmp.
+ require'lspconfig'.pyright.setup{}
+ require'lspconfig'.clangd.setup{}
+
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.select_next_item(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+  --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]]-- 
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+
+  })
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['pyright'].setup {
+    capabilities = capabilities
+  }
+  require('lspconfig')['clangd'].setup {
+    capabilities = capabilities
+  }
